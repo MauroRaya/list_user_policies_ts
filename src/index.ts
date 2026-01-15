77b0ae873534599154cfefd7d8d804610bd79fec
MauroRaya <maurorayabr@gmail.com>
2026-01-15 12:18:06 -0300

    fix and feat: use es modules and add list group names for user function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log('Hello world');
+import { 
+  IAMClient, 
+  ListGroupsForUserCommand, 
+  type ListGroupsForUserRequest 
+} from '@aws-sdk/client-iam';
+
+// list user policies
+// list user attached policies
+// list user groups
+
+async function listGroupNamesForUser(
+  client: IAMClient,
+  userName: string
+) {
+  const input: ListGroupsForUserRequest = {
+    UserName: userName
+  }
+  const command = new ListGroupsForUserCommand(input);
+  const response = await client.send(command);
+
+  const groups = response['Groups'] ?? [];
+  return groups.map(group => group.GroupName);
+}
+
+async function main() {
+  const client = new IAMClient();
+  const groups = await listGroupNamesForUser(client, 'foo');
+  console.log(groups);
+}
+
+main();
