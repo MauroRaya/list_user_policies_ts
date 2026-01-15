9be4a9c2b638dfeb78d7f11797955083a3364383
MauroRaya <maurorayabr@gmail.com>
2026-01-15 12:36:41 -0300

    fix and feat: use es modules and add list user policy names function

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,43 @@
 import { 
   IAMClient, 
   ListGroupsForUserCommand, 
-  type ListGroupsForUserRequest 
+  ListUserPoliciesCommand, 
+  type ListGroupsForUserCommandInput, 
+  type ListUserPoliciesCommandInput
 } from '@aws-sdk/client-iam';
 
 // list user policies
 // list user attached policies
 // list user groups
 
+async function listUserPolicyNames(
+  client: IAMClient,
+  userName: string
+): Promise<string[] | undefined> {
+  const input: ListUserPoliciesCommandInput = { UserName: userName };
+  const command = new ListUserPoliciesCommand(input);
+  const response = await client.send(command);
+  return response.PolicyNames;
+}
+
 async function listGroupNamesForUser(
   client: IAMClient,
   userName: string
-) {
-  const input: ListGroupsForUserRequest = {
-    UserName: userName
-  }
+): Promise<(string | undefined)[] | undefined> {
+  const input: ListGroupsForUserCommandInput = { UserName: userName };
   const command = new ListGroupsForUserCommand(input);
   const response = await client.send(command);
-
-  const groups = response['Groups'] ?? [];
-  return groups.map(group => group.GroupName);
+  return response.Groups?.map(group => group.GroupName);
 }
 
 async function main() {
   const client = new IAMClient();
-  const groups = await listGroupNamesForUser(client, 'foo');
-  console.log(groups);
+
+  const policyNames = await listUserPolicyNames(client, 'foo')
+  console.log(policyNames);
+
+  // const groups = await listGroupNamesForUser(client, 'foo');
+  // console.log(groups);
 }
 
 main();
